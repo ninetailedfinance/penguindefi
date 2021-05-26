c045c6ef70c86ea5faf52f1e118db9f7a0ec2c4f
ninetailswap <ninetailswap@gmail.com>
2021-05-26 19:15:12 +0700

    V1
    deploy

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="react-scripts" />
+
+// interface WindowChain {
+//     ethereum?: {
+//       isMetaMask?: true
+//       request?: (...args: any[]) => void
+//     }
+//   }
+  
